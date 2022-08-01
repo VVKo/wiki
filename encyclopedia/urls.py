70b0d09ec5cd6eebedcedb40d9885033c3361b24
VVKo <v.v.kovdrysh@gmail.com>
2022-08-01 10:37:17 +0300

    add entry, fix index, add markdown2

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("wiki/<TITLE>", views.entry, name="entry")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import Http404
+import markdown2
 
 from . import util
 
@@ -8,3 +10,11 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
+def entry(request, TITLE):
+    if TITLE not in util.list_entries():
+        raise Http404
+    return render(request, "encyclopedia/entry.html", {
+        "entry": markdown2.markdown(util.get_entry(TITLE)),
+        "title": TITLE
+    })
